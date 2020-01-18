e4c8c21880af6f51ad69a160c093c80e5fffb3de
dOONYGORGS <49256848+dOONYGORGS@users.noreply.github.com>
2020-01-18 15:22:46 +1000

    Complete.

---
+++ PushCounter/PushCounter/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PushCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickMeButton.Location = new System.Drawing.Point(187, 251);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(165, 56);
+            this.clickMeButton.TabIndex = 0;
+            this.clickMeButton.Text = "Click Me";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.clickMeButton_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(104, 132);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(338, 29);
+            this.name.TabIndex = 1;
+            this.name.Text = "The button has been clicked";
+            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.Location = new System.Drawing.Point(223, 186);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(95, 29);
+            this.counterLabel.TabIndex = 2;
+            this.counterLabel.Text = "0 times";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Enabled = false;
+            this.resetButton.Location = new System.Drawing.Point(187, 438);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(165, 53);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 677);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.clickMeButton);
+            this.Name = "Form1";
+            this.Text = "Click Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label counterLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ PushCounter/PushCounter/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PushCounter
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clickMeButton_Click(object sender, EventArgs e)
+        {
+            counter += 1;
+            counterLabel.Text = string.Format("{0} times", counter);
+
+            if (counter > 0)
+            {
+                resetButton.Enabled = true;
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        { 
+
+            counter = 0;
+            counterLabel.Text = string.Format("{0} times", counter);
+            resetButton.Enabled = false;
+        }
+    }
+}
